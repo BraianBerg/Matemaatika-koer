d4b171c3aff81ddf12b85f9275a4413ffea892fd
braian bergmann <braian.bergmann@gmail.com>
2023-01-09 16:33:08 +0200

    tehete lisamine

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MatemaatilineKoer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Matemaatiline Koer");
+
+            Console.Write("Palun sisesta esimene arv: ");
+            float firstNr = int.Parse(Console.ReadLine());
+
+            Console.Write("Palun sisesta tehe: ");
+            string operation = Console.ReadLine();
+
+            Console.Write("Palun sisesta teine arv: ");
+            float secondNr = int.Parse(Console.ReadLine());
+
+
+
+            try
+            {
+                switch (operation)
+                {
+                    case "+":
+                        float resultAdd = firstNr + secondNr;
+                        Console.WriteLine("Kahe numbri liitmisel on vastus: " + resultAdd);
+                        break;
+                    case "-":
+                        float resultSubtract = firstNr - secondNr;
+                        Console.WriteLine("Kahe numbri lahutamise on vastus: " + resultSubtract);
+                        break;
+                    case "*":
+                        float resultMultiply = firstNr * secondNr;
+                        Console.WriteLine("Kahe numbri korrutamise on vastus: " + resultMultiply);
+                        break;
+                    case "/":
+                        float resultDivide = firstNr / secondNr;
+                        Console.WriteLine("Kahe numbri jagamise on vastus: " + resultDivide);
+                        break;
+                    default:
+                        break;
+
+                        //teha korrutamine, jagamine ja lahutamine
+                }
+            }
+            catch (FormatException)//numbrilise v''rtuse kontroll
+            {
+                Console.WriteLine("Vale numbri formaat. Kas sa ei saa siis aru.");
+                //throw;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR. Sisestatud number on liiga suur.");
+            }
+        }
+    }
+}
